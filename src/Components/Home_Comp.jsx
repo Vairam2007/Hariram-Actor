5ef530a96e049924ab443000ed7f7e015e14f41c
Vairam2007 <vairamuthu21072007@gmail.com>
2024-07-23 11:58:45 +0530

    logo

--- src/Components/Home_Comp.jsx
+++ src/Components/Home_Comp.jsx
@@ -5,7 +5,7 @@ import home_img from "../../public/Images/homeimg.jpeg"
 const Home_Comp = () => {
   return (
     <>
-    <div id='Home' className="p-[50px] w-full justify-center items-center min-h-[90vh] bg-gray-800 text-white flex flex-col md:flex-row ">
+    <div id='Home' className="p-[50px] w-full justify-center items-center min-h-[90vh] bg-gray-800 text-white flex flex-col md:flex-row overflow-hidden ">
       <div className="w-[80%] h-[80%] flex justify-center items-center gap-[50px] flex-col md:flex-row ">
         <div data-aos="fade-right" className="w-full h-[80%]  md:w-[50%] flex justify-center items-center">
           <img style={{boxShadow:"5px 5px 10px black"}} className='w-[270px] h-[345px] md:w-[300px] md:h-[500px] rounded-[30px]' src={home_img} alt="home_img" />        </div>
